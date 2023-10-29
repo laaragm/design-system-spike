064036ffc7e8726d7c6e8dca92a00ac000c205eb
Lara <laragalvanim@gmail.com>
2023-10-29 20:48:07 -0300

    Adjust some pages of the docs

--- packages/react/src/docs/components/colors-grid.tsx
+++ packages/react/src/docs/components/colors-grid.tsx
@@ -1,35 +1,11 @@
 import { getContrast } from "polished";
 import { baseColors, core } from "@larag-ui/tokens";
 
+import "../styles/colors-grid.css";
+
 interface ColorsGridProps {
     type: "base" | "core";
 }
-
-interface ColorBoxProps {
-    name: string;
-    value: string;
-}
-
-const colorBoxStyle = {
-    border: "1px solid #000000",
-    padding: "1.5rem",
-    margin: "1rem 0",
-};
-
-const getBoxStyles = (bgColor: string) => ({
-    color: getContrast(bgColor, "#FFF") < 3 ? "#000" : "#FFF",
-    display: "flex",
-    justifyContent: "space-between",
-});
-
-const ColorBox: React.FC<ColorBoxProps> = ({ name, value }) => (
-    <div style={{ ...colorBoxStyle, backgroundColor: value }}>
-        <div style={getBoxStyles(value)}>
-            <strong>${name}</strong>
-            <span>{value}</span>
-        </div>
-    </div>
-);
 
 export function ColorsGrid({ type }: ColorsGridProps) {
     const colors = type === "core" ? core : baseColors;
@@ -37,8 +13,22 @@ export function ColorsGrid({ type }: ColorsGridProps) {
     return (
         <div>
             {Object.entries(colors).map(([key, value]) => (
-                <ColorBox key={key} name={key} value={value} />
+                <ColorBox key={key} colorKey={key} colorValue={value} />
             ))}
         </div>
     );
 }
+
+interface ColorBoxProps {
+    colorKey: string;
+    colorValue: string;
+}
+
+const ColorBox: React.FC<ColorBoxProps> = ({ colorKey, colorValue }) => (
+    <div className="color-box" style={{ backgroundColor: colorValue }}>
+        <div className={`color-content ${getContrast(colorValue, "#FFF") < 3 ? "dark-text" : "light-text"}`}>
+            <strong>${colorKey}</strong>
+            <span>{colorValue}</span>
+        </div>
+    </div>
+);
