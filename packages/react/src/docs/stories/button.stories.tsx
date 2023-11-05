28b8d224e8772ea3e203f541ded11d828b77ba4d
Lara <laragalvanim@gmail.com>
2023-11-05 12:14:39 -0300

    Adjust some details

--- packages/react/src/docs/stories/button.stories.tsx
+++ packages/react/src/docs/stories/button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button, ButtonProps } from "../../index";
 
 export default {
-    type: "Button", // page title
+    type: "Button",
     component: Button,
     args: {
         children: "Test",
