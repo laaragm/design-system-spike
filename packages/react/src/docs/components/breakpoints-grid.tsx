89a2bb979b5e8854b806cc88f3c3da782b5f6348
Lara <laragalvanim@gmail.com>
2023-11-05 11:22:14 -0300

    Add documentation for breakpoints

---
+++ packages/react/src/docs/components/breakpoints-grid.tsx
@@ -0,0 +1,23 @@
+import { breakpoints } from "@larag-ui/tokens";
+import "../styles/breakpoints-grid.css";
+
+export const BreakpointsGrid = () => {
+    return (
+        <table className="breakpoints-table">
+            <thead>
+                <tr>
+                    <th>Breakpoint</th>
+                    <th>Minimum Width</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Object.entries(breakpoints).map(([device, minWidth]) => (
+                    <tr key={device}>
+                        <td>{device.charAt(0).toUpperCase() + device.slice(1)}</td>
+                        <td>{minWidth}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
